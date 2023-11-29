23604cf00c406e2bddd46aaa1a1dbc05f3546ead
Duvier-Reinosa <duvier.reinosa@integ.ro>
2023-11-28 21:56:08 -0500

    feature(Login, Register)[Added form and backend for make it run]

---
+++ components/Login/Login.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+
+import { useState } from 'react';
+import styles from './login.module.css';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+function Login() {
+    const router = useRouter();
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleRegister = () => {
+        router.push('/register');
+    }
+
+    const handleLogin = async () => {
+        const response = await axios.post('/api/user/login', user);
+
+        if (response.status === 200) {
+            console.log('Usuario logueado');
+
+            localStorage.setItem('copaUSer', JSON.stringify(response.data));
+            router.push('/home');
+        } else {
+            alert('Error al loguear usuario');
+        }
+    }
+
+    return (
+        <div className={styles.loginContainer}>
+            <div className={styles.loginFormContainer}>
+                <h1>Login</h1>
+                <input className={styles.loginInput} type="email" placeholder="email" onChange={ev => setUser({ ...user, email: ev.target.value })} />
+                <input className={styles.loginInput} type="password" placeholder="password" onChange={ev => setUser({ ...user, password: ev.target.value })} />
+                <button className={styles.submmit} onClick={handleLogin}>Login</button>
+                <div>
+                    <p>No tienes una cuenta?</p>
+                    <button className={styles.submmit} onClick={handleRegister}>Regístrate</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,12 @@
 import { useState, useEffect } from "react";
-// import "../styles/globals.css";
-import { supabase } from "../utils/supabase";
+import './globals.css'
 
 function MyApp({ Component, pageProps }) {
- const [session, setSession] = useState(null);
-
- useEffect(() => {
-    console.log("MyApp");
-  //  setSession(supabase.auth.session());
-  //  supabase.auth.onAuthStateChange((_event, session) => {
-  //   console.log( {session})
-  //    setSession(session);
-  //  });
- }, []);
 
  return (
-   <div>
-        <Component {...pageProps} session={session} />
-   </div>
+    <div>
+      <Component {...pageProps} session={pageProps.session} />
+    </div>
  );
 }
 export default MyApp;
