e108bf1ad2bc194c8315970c8dc0fb4dc55deca0
Duvier-Reinosa <duvier.reinosa@integ.ro>
2023-11-28 22:08:59 -0500

    feature(_app.tsx)[Added props type for file]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import './globals.css'
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
 
  return (
     <div>
